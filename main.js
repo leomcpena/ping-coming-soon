bc04c8dd6936e31aa835484c5820582b45dceba4
leomcpena <134736978+leomcpena@users.noreply.github.com>
2023-08-02 19:27:16 -0300

    add success message

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const form = document.getElementById("form");
 const email = document.getElementById("email");
 const errorDisplay = document.getElementById("error-message");
+const successDisplay = document.getElementById("success-message");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -10,18 +11,21 @@ form.addEventListener("submit", (e) => {
 
 const setError = (message) => {
   errorDisplay.innerText = message;
+  successDisplay.innerHTML = "";
   email.classList.add("error");
 };
 
 const setSuccess = () => {
   errorDisplay.innerText = "";
+  successDisplay.innerHTML = `<h2>Thanks! You'll be notified as soon as <span class="clr-primary-900">PING<span class="clr-primary-400">.</span></span> launches!</h2>`;
   email.classList.remove("error");
+  form.classList.add("hidden");
 };
 
-const isValidEmail = (email) => {
+const isValidEmail = (value) => {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  return re.test(String(value).toLowerCase());
 };
 
 const validateEmail = () => {
